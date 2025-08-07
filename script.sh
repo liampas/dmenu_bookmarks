18a602bdf38ba0111eadae2647d7b0658212a317
liampas <liam.plamondon@gmail.com>
2025-08-07 15:32:36 -0400

    yeh

--- script.sh
+++ script.sh
@@ -5,12 +5,16 @@ OUTDIR="bookmarks_files"
 
 mkdir -p "$OUTDIR"
 
-sqlite3 -nullvalue '' -batch "$DB" "SELECT title FROM moz_bookmarks WHERE title IS NOT NULL;" | while IFS= read -r title; do
-    # skip empty titles just in case
+sqlite3 -readonly -separator $'\t' "$DB" "
+SELECT b.title, COALESCE(a.content, '')
+FROM moz_bookmarks b
+LEFT JOIN moz_items_annos a ON a.item_id = b.id
+LEFT JOIN moz_anno_attributes aa ON aa.id = a.anno_attribute_id
+WHERE b.title IS NOT NULL AND (aa.name = 'bookmarkProperties/description' OR aa.name IS NULL);
+" | while IFS=$'\t' read -r title desc; do
     [[ -z "$title" ]] && continue
-    # sanitize filename
     safe_name=$(echo "$title" | tr -cd '[:alnum:]._-')
-    # create empty file
-    touch "$OUTDIR/$safe_name"
+    # Avoid empty or duplicate filenames
+    [[ -z "$safe_name" ]] && safe_name="bookmark_$RANDOM"
+    echo "$desc" > "$OUTDIR/$safe_name"
 done
-
